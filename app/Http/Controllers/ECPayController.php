417204a3ffcb5b986e14bcf44624d48cc40ddf33
leeshiawoei <jonathan850629@hotmail.com>
2018-11-19 13:50:10 +0800

    '20181119Ecpay'

--- app/Http/Controllers/ECPayController.php
+++ app/Http/Controllers/ECPayController.php
@@ -28,89 +28,72 @@ class ECPayController extends Controller
         $MerchantTradeDate=date('Y/m/d H:i:s');
 
         //基本參數(請依系統規劃自行調整)
-        // Ecpay::i()->Send['ReturnURL'] = "https://livego.herokuapp.com/checkout_return";
-        // Ecpay::i()->Send['ClientBackURL'] = "https://livego.herokuapp.com/buyer_index" ; 
-        // Ecpay::i()->Send['MerchantTradeNo'] =$MerchantTradeNo; //訂單編號
-        // Ecpay::i()->Send['MerchantTradeDate'] =$MerchantTradeDate; //交易時間
-        // Ecpay::i()->Send['TotalAmount'] = $TotalAmount; //交易金額
-        // Ecpay::i()->Send['TradeDesc'] = $page_name; //交易描述
-        // Ecpay::i()->Send['ChoosePayment'] = $this->GetPaymentWay($request->payway); //付款方式
+        Ecpay::i()->Send['ReturnURL'] = "https://livego.herokuapp.com/checkout_return";
+        Ecpay::i()->Send['ClientBackURL'] = "https://livego.herokuapp.com/buyer_index" ; 
+        Ecpay::i()->Send['MerchantTradeNo'] =$MerchantTradeNo; //訂單編號
+        Ecpay::i()->Send['MerchantTradeDate'] =$MerchantTradeDate; //交易時間
+        Ecpay::i()->Send['TotalAmount'] = $TotalAmount; //交易金額
+        Ecpay::i()->Send['TradeDesc'] = $page_name; //交易描述
+        Ecpay::i()->Send['ChoosePayment'] = $this->GetPaymentWay($request->payway); //付款方式
         
         //加密
         //step1、2
-        // $sMacValue=
-        // 'HashKey=5294y06JbISpM5x9&ChoosePayment=ALL&EncryptType=1&ItemName=';
+        $sMacValue=
+        'HashKey=5294y06JbISpM5x9&ChoosePayment=ALL&EncryptType=1&ItemName=';
 
-        // $page_id='';
-        // $buyer_id='';
-        // //訂單的商品資料
-        // foreach($order_detail as $order)
-        // {
-        //     array_push(Ecpay::i()->Send['Items'], array('Name' =>  $order->goods_name, 'Price' => (int) ( $order->goods_price),
-        //     'Currency' => "元", 'Quantity' => (int) ( $order->goods_num), 'URL' => "dedwed"));
-        //     $page_id=$order->page_id;
-        //     $buyer_id=$order->fb_id;
-        //     $sMacValue=$sMacValue.'#'. $order->goods_name;
+        $page_id='';
+        $buyer_id='';
+        //訂單的商品資料
+        foreach($order_detail as $order)
+        {
+            array_push(Ecpay::i()->Send['Items'], array('Name' =>  $order->goods_name, 'Price' => (int) ( $order->goods_price),
+            'Currency' => "元", 'Quantity' => (int) ( $order->goods_num), 'URL' => "dedwed"));
+            $page_id=$order->page_id;
+            $buyer_id=$order->fb_id;
+            $sMacValue=$sMacValue.'#'. $order->goods_name;
             
-        // }
+        }
 
-        // $sMacValue=$sMacValue.
-        // '&MerchantID=2000132&MerchantTradeDate='.$MerchantTradeDate.
-        // '&MerchantTradeNo='.$MerchantTradeNo.
-        // '&PaymentType=aio&ReturnURL=livego.herokuapp.com/checkout_return&TotalAmount='.$TotalAmount.
-        // '&TradeDesc='.$page_name.
-        // '&HashIV=v77hoKGq4kWxNNIS';
-        // //step3
-        // $sMacValue=urlencode($sMacValue);        
-        // //step4
-        // $sMacValue = strtolower($sMacValue);
-        // //step5
-        // $sMacValue = str_replace('%2d', '-', $sMacValue);
-        // $sMacValue = str_replace('%5f', '_', $sMacValue);
-        // $sMacValue = str_replace('%2e', '.', $sMacValue);
-        // $sMacValue = str_replace('%21', '!', $sMacValue);
-        // $sMacValue = str_replace('%2a', '*', $sMacValue);
-        // $sMacValue = str_replace('%28', '(', $sMacValue);
-        // $sMacValue = str_replace('%29', ')', $sMacValue);
-        // $sMacValue = str_replace('%20', '+', $sMacValue);
-        // //step6        
-        // $sMacValue=hash('sha256', $sMacValue);
-        // //step7
-        // $sMacValue = strtoupper($sMacValue);
+        $sMacValue=$sMacValue.
+        '&MerchantID=2000132&MerchantTradeDate='.$MerchantTradeDate.
+        '&MerchantTradeNo='.$MerchantTradeNo.
+        '&PaymentType=aio&ReturnURL=livego.herokuapp.com/checkout_return&TotalAmount='.$TotalAmount.
+        '&TradeDesc='.$page_name.
+        '&HashIV=v77hoKGq4kWxNNIS';
+        //step3
+        $sMacValue=urlencode($sMacValue);        
+        //step4
+        $sMacValue = strtolower($sMacValue);
+        //step5
+        $sMacValue = str_replace('%2d', '-', $sMacValue);
+        $sMacValue = str_replace('%5f', '_', $sMacValue);
+        $sMacValue = str_replace('%2e', '.', $sMacValue);
+        $sMacValue = str_replace('%21', '!', $sMacValue);
+        $sMacValue = str_replace('%2a', '*', $sMacValue);
+        $sMacValue = str_replace('%28', '(', $sMacValue);
+        $sMacValue = str_replace('%29', ')', $sMacValue);
+        $sMacValue = str_replace('%20', '+', $sMacValue);
+        //step6        
+        $sMacValue=hash('sha256', $sMacValue);
+        //step7
+        $sMacValue = strtoupper($sMacValue);
 
-        // //insert DB
-        // $OrderDetail = new OrderDetail();
-        // $OrderDetail->page_id = $page_id;
-        // $OrderDetail->page_name = $page_name;
-        // $OrderDetail->buyer_fbid = $buyer_id;
-        // $OrderDetail->buyer_name = $buyer_name;
-        // $OrderDetail->order_id = $MerchantTradeNo;
-        // $OrderDetail->transaction_date = $MerchantTradeDate;
-        // $OrderDetail->status = '0';
-        // $OrderDetail->mac_value = $sMacValue;
-        // $OrderDetail->note = $note;
-        // $OrderDetail->total_price = $TotalAmount;
-        // $OrderDetail->buyer_address = $address;
-        // $OrderDetail->buyer_phone = $phone;
-        // $OrderDetail->save();
+        //insert DB
+        $OrderDetail = new OrderDetail();
+        $OrderDetail->page_id = $page_id;
+        $OrderDetail->page_name = $page_name;
+        $OrderDetail->buyer_fbid = $buyer_id;
+        $OrderDetail->buyer_name = $buyer_name;
+        $OrderDetail->order_id = $MerchantTradeNo;
+        $OrderDetail->transaction_date = $MerchantTradeDate;
+        $OrderDetail->status = '0';
+        $OrderDetail->mac_value = $sMacValue;
+        $OrderDetail->note = $note;
+        $OrderDetail->total_price = $TotalAmount;
+        $OrderDetail->buyer_address = $address;
+        $OrderDetail->buyer_phone = $phone;
+        $OrderDetail->save();
 
-        // //Go to EcPay
-        // echo "緑界頁面導向中...";
-        // echo Ecpay::i()->CheckOutString();
-
-        Ecpay::i()->Send['ReturnURL']         = "http://livego.herokuapp.com/payReturn" ;
-        Ecpay::i()->Send['MerchantTradeNo']   = "Test".time() ;           //訂單編號
-        Ecpay::i()->Send['MerchantTradeDate'] = date('Y/m/d H:i:s');      //交易時間
-        Ecpay::i()->Send['TotalAmount']       = 2000;                     //交易金額
-        Ecpay::i()->Send['TradeDesc']         = "good to drink" ;         //交易描述
-       // Ecpay::i()->Send['ChoosePayment']     = \ECPay_PaymentMethod::ALL ;     //付款方式
-        
-   
-  
-          
-        array_push(Ecpay::i()->Send['Items'], array('Name' => "緑界黑芝麻豆漿", 'Price' => (int)"2000",
-        'Currency' => "元", 'Quantity' => (int) "1", 'URL' => "dedwed"));
-  
         //Go to EcPay
         echo "緑界頁面導向中...";
         echo Ecpay::i()->CheckOutString();
@@ -120,64 +103,56 @@ class ECPayController extends Controller
 
     public function payReturn(Request $request)
     {
-       
-        // $input = Input::all();
-        // $MerchantID=$request->input('MerchantID');
-        // $MerchantTradeNo=$request->input('MerchantTradeNo');
-        // $RtnCode=$request->input('RtnCode');
-        // $RtnMsg=$request->input('RtnMsg');
-        // $TradeNo=$request->input('TradeNo');
-        // $TradeAmt=$request->input('TradeAmt');
-        // $PaymentDate=$request->input('PaymentDate');
-        // $PaymentType=$request->input('PaymentType');
-        // $PaymentTypeChargeFee=$request->input('PaymentTypeChargeFee');
-        // $TradeDate=$request->input('TradeDate');
-        // $SimulatePaid=$request->input('SimulatePaid');
-        // $CheckMacValue=$request->input('CheckMacValue');
-
         $arFeedback = Ecpay::i()->CheckOutFeedback($request->all());
-        print Ecpay::i()->getResponse($arFeedback);
+        $return_status = Ecpay::i()->getResponse($arFeedback);
+
+
+        $MerchantID = $request->input('MerchantID');
+        $MerchantTradeNo = $request->input('MerchantTradeNo');
+        $RtnCode = $request->input('RtnCode');
+        $RtnMsg = $request->input('RtnMsg');
+        $TradeNo=$request->input('TradeNo');
+        $TradeAmt=$request->input('TradeAmt');
+        $PaymentDate=$request->input('PaymentDate');
+        $PaymentType=$request->input('PaymentType');
+        $PaymentTypeChargeFee=$request->input('PaymentTypeChargeFee');
+        $TradeDate=$request->input('TradeDate');
+        $SimulatePaid=$request->input('SimulatePaid');
+        $CheckMacValue=$request->input('CheckMacValue');
+
+         
 
         $OrderDetail = new OrderDetail();
-       
-        $OrderDetail->page_id = 'test';
-        //$OrderDetail->page_id = $MerchantID;
-        // $OrderDetail->page_name = $MerchantTradeNo;
-        // $OrderDetail->buyer_fbid = $RtnCode;
-        // $OrderDetail->buyer_name = $RtnMsg;
-        // $OrderDetail->order_id = $TradeNo;
-        // $OrderDetail->transaction_date = $TradeAmt;
-        // $OrderDetail->status = $PaymentDate;
-        // $OrderDetail->mac_value = $PaymentType;
-        // $OrderDetail->note = $PaymentTypeChargeFee;
-        // $OrderDetail->total_price = $TradeDate;
-        // $OrderDetail->buyer_address = $SimulatePaid;
-        // $OrderDetail->buyer_phone = $CheckMacValue;
+
+        $OrderDetail->page_id = $MerchantID;
+        $OrderDetail->page_name = $MerchantTradeNo;
+        $OrderDetail->buyer_fbid = $RtnCode;
+        $OrderDetail->buyer_name = $RtnMsg;
+        $OrderDetail->order_id = $TradeNo;
+        $OrderDetail->transaction_date = $TradeAmt;
+        $OrderDetail->status = $PaymentDate;
+        $OrderDetail->mac_value = $PaymentType;
+        $OrderDetail->note = $PaymentTypeChargeFee;
+        $OrderDetail->total_price = $TradeDate;
+        $OrderDetail->buyer_address = $SimulatePaid;
+        $OrderDetail->buyer_phone = $CheckMacValue;
         $OrderDetail->save();
 
       
 
 
-        // $sMacValue=OrderDetail::where('status', '=', '0')
-        //          ->where('order_id', '=',$TradeNo ) 
-        //          ->select('mac_value')
-        //          ->get();
+        $sMacValue=OrderDetail::where('status', '=', '0')
+                 ->where('order_id', '=',$TradeNo ) 
+                 ->select('mac_value')
+                 ->get();
 
-        // if($CheckMacValue==$sMacValue)
-        // {
-        //     return '1|OK';
-        // }
+        if($CheckMacValue==$sMacValue)
+        {
+            return '1|OK';
+        }
 
         
-        //輸入通知
-        // notification::create([
-        //     'email'=>$project -> id,
-        //     'img'=>'/outImg/'.$project->out_pic_path,
-        //     'title'=>$title,
-        //     'detail'=>$detail,
-        //     'status'=>"0"
-        // ]);
-
+       
     }  
 
       
