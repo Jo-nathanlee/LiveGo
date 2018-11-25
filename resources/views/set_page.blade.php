e2c0b72ecc8ee3c3c7096028951110fb720ce4af
leeshiawoei <jonathan850629@hotmail.com>
2018-11-26 00:02:36 +0800

    '20181125'

--- resources/views/set_page.blade.php
+++ resources/views/set_page.blade.php
@@ -18,6 +18,29 @@
         {{ session('alert') }}
     </div> -->
     <script>
+    function message_danger() {
+                // error_code 接收錯誤代碼 error_msg 接收錯誤提示訊息
+                var alert_div = document.createElement("div");
+                alert_div.setAttribute('id', 'data_info');
+                alert_div.setAttribute("class", "card-body align-middle h5 text-center bg-light");
+                alert_div.innerHTML =
+                    "<strong><i class='icofont icofont-exclamation-circle h1'></i> </strong><div class='mt-4'>直播尚未開起，請先開起直播</div>";
+                var warp_div = document.createElement("div");
+
+                warp_div.setAttribute("class", "card shadow show_msg_center  w-25 bg-light")
+                warp_div.append(alert_div);
+                $("html").append(warp_div);
+
+                setTimeout(
+                    function () {
+                        $("#data_info").fadeToggle(1000);
+                    }, 2000);
+                setTimeout(
+                    function () {
+                        $("#data_info").parent().remove();
+                    }, 3000);
+            }
+
         message_danger();
     </script>
 @endif       
