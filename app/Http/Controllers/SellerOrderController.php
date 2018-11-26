eca69584ae5422d8421d111b8faa7f0af9da4af5
leeshiawoei <jonathan850629@hotmail.com>
2018-11-26 23:49:57 +0800

    '20181126'

--- app/Http/Controllers/SellerOrderController.php
+++ app/Http/Controllers/SellerOrderController.php
@@ -145,7 +145,6 @@ class SellerOrderController extends Controller
      public function downloadPDF(Request $request)
      {
          $order = $request->order;
-         dd($order);
          $output = '';
          $order_status='';
          $created_time='';
