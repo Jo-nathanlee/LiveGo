56bbb35f05f374bc08fa2b3ff5274f976d22445b
leeshiawoei <jonathan850629@hotmail.com>
2018-11-27 00:36:08 +0800

    '20181126'

--- app/Http/Controllers/SellerOrderController.php
+++ app/Http/Controllers/SellerOrderController.php
@@ -17,43 +17,43 @@ class SellerOrderController extends Controller
      //賣家訂單查看------------------------------------------------------------------------
      public function SellerOrderAll(Request $request)//全部訂單
      {
-      //   if (Gate::allows('seller-only',  Auth::user())) {
-      //       $page = Page::where('fb_id', Auth::user()->fb_id)->first();
-      //       $page_id = $page->page_id;
-      //       $query = CheckoutOrder::all()
-      //               ->where('page_id', '=', $page_id)
-      //               ->groupBy('order_id');
-      //       $countAllOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->distinct()
-      //       ->count();
-      //       $countUnpaidOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->where('order_status', '=', 'unpaid')
-      //       ->distinct()
-      //       ->count();
-      //       $countUndeliveredOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->where('order_status', '=', 'undelivered')
-      //       ->distinct()
-      //       ->count();   
-      //       $countDeliveredOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->where('order_status', '=', 'delivered')
-      //       ->distinct()
-      //       ->count();     
-      //       $countFinishedOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->where('order_status', '=', 'finished')
-      //       ->distinct()
-      //       ->count();  
-      //       $countCanceledOrder=CheckoutOrder::where('page_id', '=', $page_id)
-      //       ->where('order_status', '=', 'canceled')
-      //       ->distinct()
-      //       ->count();          
+        if (Gate::allows('seller-only',  Auth::user())) {
+            $page = Page::where('fb_id', Auth::user()->fb_id)->first();
+            $page_id = $page->page_id;
+            $query = CheckoutOrder::all()
+                    ->where('page_id', '=', $page_id)
+                    ->groupBy('order_id');
+            $countAllOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->distinct()
+            ->count();
+            $countUnpaidOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->where('order_status', '=', 'unpaid')
+            ->distinct()
+            ->count();
+            $countUndeliveredOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->where('order_status', '=', 'undelivered')
+            ->distinct()
+            ->count();   
+            $countDeliveredOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->where('order_status', '=', 'delivered')
+            ->distinct()
+            ->count();     
+            $countFinishedOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->where('order_status', '=', 'finished')
+            ->distinct()
+            ->count();  
+            $countCanceledOrder=CheckoutOrder::where('page_id', '=', $page_id)
+            ->where('order_status', '=', 'canceled')
+            ->distinct()
+            ->count();          
                         
     
-      //       return view('seller_order', ['order' => $query,'countAllOrder' => $countAllOrder,'countUnpaidOrder' => $countUnpaidOrder,'countUndeliveredOrder' => $countUndeliveredOrder,'countDeliveredOrder' => $countDeliveredOrder,'countFinishedOrder' => $countFinishedOrder,'countCanceledOrder' => $countCanceledOrder]);
-      //   }
-      //   else
-      //   {
-      //      return redirect('/')->with('alert', '您尚未開通，請聯繫我們！');
-      //   }
+            return view('seller_order', ['order' => $query,'countAllOrder' => $countAllOrder,'countUnpaidOrder' => $countUnpaidOrder,'countUndeliveredOrder' => $countUndeliveredOrder,'countDeliveredOrder' => $countDeliveredOrder,'countFinishedOrder' => $countFinishedOrder,'countCanceledOrder' => $countCanceledOrder]);
+        }
+        else
+        {
+           return redirect('/')->with('alert', '您尚未開通，請聯繫我們！');
+        }
      }
  
      public function SellerOrderUnpaid(Request $request)//未付款訂單
