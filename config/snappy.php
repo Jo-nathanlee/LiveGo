522e34a7e1145d0da4347e521b938f841a950d8e
leeshiawoei <jonathan850629@hotmail.com>
2018-11-27 12:56:24 +0800

    '20181127'

--- config/snappy.php
+++ config/snappy.php
@@ -5,14 +5,14 @@ return array(
 
     'pdf' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf"',
+        'binary'  =>  'wkhtmltopdf',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary'  => '"C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage"',
+        'binary'  => 'wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
