4ce1ae7336b41531900ef35ae1a1b8cec1f9378c
leeshiawoei <jonathan850629@hotmail.com>
2018-11-27 18:51:20 +0800

    '20181127'

--- app/Http/Controllers/SellerOrderController.php
+++ app/Http/Controllers/SellerOrderController.php
@@ -312,15 +312,15 @@ class SellerOrderController extends Controller
          $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
          // set document information
          $pdf->SetTitle('訂單');
-         //$pdf->SetHeaderData('', 14,'訂單', '訂單');
+         $pdf->SetHeaderData('', 14,'訂單', '訂單');
          // set header and footer fonts
-         //$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+         $pdf->setHeaderFont(Array('msungstdlight', '', PDF_FONT_SIZE_MAIN));
          $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
          // set default monospaced font
          $pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
          // set margins
          $pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-         // $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+         $pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
          $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
          // set auto page breaks
          $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
