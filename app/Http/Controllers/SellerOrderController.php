3b8bbf6789c21b140838ab89f98577a7515ecd77
leeshiawoei <jonathan850629@hotmail.com>
2018-11-27 18:02:11 +0800

    '20181127'

--- app/Http/Controllers/SellerOrderController.php
+++ app/Http/Controllers/SellerOrderController.php
@@ -313,13 +313,6 @@ class SellerOrderController extends Controller
 
          $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
          // set document information
-         $pdf->SetCreator(PDF_CREATOR);
-         $pdf->SetAuthor('Nicola Asuni');
-         $pdf->SetTitle('TCPDF Example 038');
-         $pdf->SetSubject('TCPDF Tutorial');
-         $pdf->SetKeywords('TCPDF, PDF, example, test, guide');
-         // set default header data
-         $pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 038', PDF_HEADER_STRING);
          // set header and footer fonts
          $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
          $pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
@@ -331,25 +324,19 @@ class SellerOrderController extends Controller
          $pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
          // set auto page breaks
          $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
-         // set image scale factor
-         $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+         
          // set some language-dependent strings (optional)
          if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
             require_once(dirname(__FILE__).'/lang/eng.php');
             $pdf->setLanguageArray($l);
          }
          // ---------------------------------------------------------
-         // set font
-         $pdf->SetFont('helvetica', '', 20);
          // add a page
          $pdf->AddPage();
-         $txt = 'Example of CID-0 CJK unembedded font.
-         To display extended text you must have CJK fonts installed for your PDF reader:';
-         $pdf->Write(0, $txt, '', 0, 'L', true, 0, false, false, 0);
-         // set font
-         $pdf->SetFont('cid0jp', '', 40);
-         $txt = 'こんにちは世界';
-         $pdf->Write(0, $txt, '', 0, 'L', true, 0, false, false, 0);
+
+         $pdf->SetFont('msungstdlight', '', 20);
+
+         $pdf->Write(0, $output, '', 0, 'L', true, 0, false, false, 0);
          // ---------------------------------------------------------
          //Close and output PDF document
          $pdf->Output('example_038.pdf', 'I');
