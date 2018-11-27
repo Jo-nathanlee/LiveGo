5ae386af592fd2abc7dcf29da5870d24c47934d4
leeshiawoei <jonathan850629@hotmail.com>
2018-11-27 17:43:05 +0800

    '20181127'

--- app/Http/Controllers/SellerOrderController.php
+++ app/Http/Controllers/SellerOrderController.php
@@ -311,15 +311,46 @@ class SellerOrderController extends Controller
 
 
          $pdf = new \TCPDF();
-         $pdf->SetTitle('訂單資訊');
+         $pdf->SetCreator('懒人开发网');
+         $pdf->SetAuthor('懒人开发网');
+         $pdf->SetTitle('TCPDF示例');
+         $pdf->SetSubject('TCPDF示例');
+         $pdf->SetKeywords('TCPDF, PDF, PHP');
+ 
+         // 设置页眉和页脚信息
+         $pdf->SetHeaderData('tcpdf_logo.jpg', 30, 'LanRenKaiFA.com', '学会偷懒，并懒出效率！', [0, 64, 255], [0, 64, 128]);
+         $pdf->setFooterData([0, 64, 0], [0, 64, 128]);
+ 
+         // 设置页眉和页脚字体
+         $pdf->setHeaderFont(['stsongstdlight', '', '10']);
+         $pdf->setFooterFont(['helvetica', '', '8']);
+ 
+         // 设置默认等宽字体
          $pdf->SetDefaultMonospacedFont('courier');
-         $pdf->SetMargins(15, 15, 15);
-         //$pdf->SetAutoPageBreak(true, 25);
+ 
+         // 设置间距
+         $pdf->SetMargins(15, 15, 15);//页面间隔
+         $pdf->SetHeaderMargin(5);//页眉top间隔
+         $pdf->SetFooterMargin(10);//页脚bottom间隔
+ 
+         // 设置分页
+         $pdf->SetAutoPageBreak(true, 25);
+ 
+         // set default font subsetting mode
          $pdf->setFontSubsetting(true);
+ 
+         //设置字体 stsongstdlight支持中文
          $pdf->SetFont('stsongstdlight', '', 14);
+ 
+         //第一页
          $pdf->AddPage();
-         $pdf->writeHTML($output);
-
+         $pdf->writeHTML('<div style="text-align: center"><h1>第一页内容</h1></div>');
+         $pdf->writeHTML('<p>我是第一行内容</p>');
+         $pdf->writeHTML('<p style="color: red">我是第二行内容</p>');
+         $pdf->writeHTML('<p>我是第三行内容</p>');
+         $pdf->Ln(5);//换行符
+         $pdf->writeHTML('<p><a href="http://www.lanrenkaifa.com/" title="">懒人开发网</a></p>');
+ 
          return $pdf->Output('t.pdf', 'I');
      }
 }
