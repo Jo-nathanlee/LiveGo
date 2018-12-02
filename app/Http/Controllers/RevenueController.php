34ce570b55746fa87e76ea838890b0eb8eaff221
leeshiawoei <jonathan850629@hotmail.com>
2018-12-02 20:56:26 +0800

    '20181202'

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
-class MallProductController extends Controller
+class RevenueController extends Controller
 {
     public function DailyRevenue(Request $request)
     {
