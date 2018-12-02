4e44d1d138fdad197f8882979441e8f8f49a1222
leeshiawoei <jonathan850629@hotmail.com>
2018-12-02 21:27:39 +0800

    '20181202'

--- app/Http/Controllers/RevenueController.php
+++ app/Http/Controllers/RevenueController.php
@@ -85,6 +85,8 @@ class RevenueController extends Controller
                 array_push($amount, $$temp2);
             }
 
+            dd($date[0]);
+
             return view('daily_revenue', ['date' => $date,'amount' => $amount]);
         }   
         else
