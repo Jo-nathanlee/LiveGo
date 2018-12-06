5468dd13d05d2e5bdc387e5277a50b50e68f1c2f
leeshiawoei <jonathan850629@hotmail.com>
2018-12-06 20:39:50 +0800

    '20181205'

---
+++ resources/views/seller_order_detail.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title','Live GO 我的訂單')
+
+   
+@section('heads')
+    <!-- 我新增的 CSS -->
+    <link rel="stylesheet" href="css/list_mgnt.css">
+     <!-- datatable + bootstrap 4  -->
+     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+@stop
+
+
