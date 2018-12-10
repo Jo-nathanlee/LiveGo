ceecc3ac729323884cb866fd091ca895d52d8dd7
leeshiawoei <jonathan850629@hotmail.com>
2018-12-10 23:08:30 +0800

    '20181210'

--- resources/views/PartialLayout/sidebar_page.blade.php
+++ resources/views/PartialLayout/sidebar_page.blade.php
@@ -15,10 +15,10 @@
                 <i class="icofont icofont-settings-alt"></i>直播商品設定</a>
         </li>
         <li>
-            <a href="#pageSubmenuㄉ" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+            <a href="#pageSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                 <i class="icofont icofont-home"></i>粉絲團設定
             </a>
-            <ul class="collapse list-unstyled" id="pageSubmenuㄉ">
+            <ul class="collapse list-unstyled" id="pageSubmenu2">
                 <li>
                     <a href="{{ route('set_page') }}">綁定粉絲團</a>
                 </li>
@@ -43,10 +43,10 @@
                 <i class="icofont icofont-male"></i>會員名單</a>
         </li>
         <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+            <a href="#pageSubmenu1" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                 <i class="icofont icofont-investigator"></i>棄標黑名單
             </a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
+            <ul class="collapse list-unstyled" id="pageSubmenu1">
                 <li>
                     <a href="{{ route('bid_blacklist') }}">棄標黑名單</a>
                 </li>
@@ -56,10 +56,10 @@
             </ul>
         </li>
         <li>
-            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+            <a href="#pageSubmenu3" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                 <i class="icofont icofont-shopping-cart"></i>來福逛逛設定
             </a>
-            <ul class="collapse list-unstyled" id="pageSubmenu">
+            <ul class="collapse list-unstyled" id="pageSubmenu3">
                 <li>
                     <a href="{{ route('AddProduct_show') }}">新增商品</a>
                 </li>
