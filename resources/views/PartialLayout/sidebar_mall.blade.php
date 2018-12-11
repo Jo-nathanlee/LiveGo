9eac3475f26030c3433542eaedd068464f52d8da
leeshiawoei <jonathan850629@hotmail.com>
2018-12-11 23:42:45 +0800

    '20181211'

--- resources/views/PartialLayout/sidebar_mall.blade.php
+++ resources/views/PartialLayout/sidebar_mall.blade.php
@@ -29,10 +29,10 @@
     </ul>
     <ul class="list-unstyled">
         <li>
-            <a>公司地址 : <br/>{{ $address }}</a>
+            <!-- <a>公司地址 : <br/>{{ $address }}</a> -->
         </li>
         <li>
-            <a>匯款帳號 : <br/> {{ $phone }} </a>
+            <!-- <a>匯款帳號 : <br/> {{ $phone }} </a> -->
         </li>
 
     </ul>
